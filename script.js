85e72c079dbaff0a0ef8d5c3a0a1860498df5c95
Your Name <you@example.com>
2023-02-03 00:36:10 -0800

    JS files updated

--- script.js
+++ script.js
@@ -10,10 +10,54 @@ var forecastContainer = document.querySelector('#forecast');
 
 var todayContainer = document.querySelector('#today');
 var searchHistoryContainer = document.querySelector('#history');
-
-// 14
-// 15 
+// Timezone plugins to day.js
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugins_timezone);
 
 function renderSearchHistory() {
     searchHistoryContainer.innerHTML = '';
+
+    // Updates history to show recent search input 
+    for (var i = searchHistory.length -1; i >= 0; i --) {
+        var btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        btn.setAttribute('today forecast');
+        btn.classList.add('btn-history');
+
+        // Search for city data by name
+        btn.setAttribute('data-search', searchHistory[i]);
+        btn.textContent = searchHistory[i];
+        searchHistoryContainer.append(btn);
+    }
+}
+
+// Update history in local storage
+function appendToHistory(search) {
+
+    if (searchHistory.indexOf(search) !== -1) {
+        return;
+    }
+    searchHistory.push(search);
+    
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory))
+}
+
+function initSearchHistory() {
+    //
+    //
+    //
+
+    //
+}
+// Function for current weathr data fieched from weather ipi
+function renderCurrentWeather(city, weather) {
+    var date = day.js().format('M/D/YYYY')
+    //
+    var tempF = weather.main.temp;
+    var windMph = weather.wind.speed;
+    var humidity =weather.main.humidity;
+    var iconUrl = `https://openweathermap.org/img/w/${weather.weather[0].icon.png}`;
+    var iconDescription =weather.weather[0].Description || weather[0].main;
+
+
 }
